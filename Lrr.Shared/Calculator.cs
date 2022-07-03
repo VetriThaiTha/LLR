26917ec3d56a832c8c1ff26d7df2c165b0529ba9
VetriThaiTha <thaithavetri@gmail.com>
2022-07-03 23:04:57 +0100

    update

--- Lrr.Shared/Calculator.cs
+++ Lrr.Shared/Calculator.cs
@@ -20,26 +20,28 @@ namespace Lrr.Shared
             var aDetails = new AuctionDetails
             {
                 EndDate = inputData.RegistrationDate.AddMonths(configuration.AuctionDuration),
-                StepValue = (int) Math.Ceiling( inputData.PurchaseValue * configuration.StepValueMultiple),
+                StepValue = (int)Math.Ceiling( inputData.PurchaseValue * configuration.StepValueMultiple),
                 StartingValue = (int)Math.Ceiling(GetWhiteValue(inputData) * configuration.StartingValueMultiple),
                 WhiteValue = GetWhiteValue(inputData) ,
-                BlackValue = inputData.PurchaseValue * ((100 - inputData.WhiteBlackRatio )/ 100.0),
+                BlackValue = (inputData.PurchaseValue - GetWhiteValue(inputData)) ,
                 
             };
 
             var validValues = new List<string>();
 
-            for (int i = 0; i < 100; i++)
+            for (uint i = 0; i < 100; i++)
             {
-                validValues.Add((aDetails.StartingValue + aDetails.StepValue * i).ToString("##\\,##\\,##\\,##0"));
+                validValues.Add((aDetails.StartingValue +  aDetails.StepValue * i).ToString("##\\,##\\,##\\,##0"));
             }
             aDetails.ValidAuctionBidValues = validValues;
             return aDetails;
         }
 
-        private double GetWhiteValue(IInputData inputData)
+        private int GetWhiteValue(IInputData inputData)
         {
-            return inputData.PurchaseValue * inputData.WhiteBlackRatio / 100.0;
+            ulong tmp = (ulong)inputData.PurchaseValue * (ulong) inputData.WhiteBlackRatio;
+            
+            return (int) (tmp / 100);
         }
     }
 }
--- Lrr.Shared/IAuctionDetails.cs
+++ Lrr.Shared/IAuctionDetails.cs
@@ -11,12 +11,15 @@ namespace Lrr.Shared
         public DateOnly EndDate { get; set; }
         public int StartingValue { get; set; }
         public int StepValue { get; set; }
-        public double WhiteValue { get; set; }
-        public double BlackValue { get; set; }
+        public int WhiteValue { get; set; }
+        public int BlackValue { get; set; }
         public List<string> ValidAuctionBidValues { get; set; }
 
         public string FmtStartingValue { get; }
         public string FmtStepValue { get; }
+
+        public string FmtWhiteValue { get; }
+        public string FmtBlackValue { get; }
 
     }
 }
