c21762ab06dbb26d0cf09442e2e30fddc8780fa5
VetriThaiTha <thaithavetri@gmail.com>
2022-07-04 22:19:45 +0100

    payment schedule updates

--- Lrr.Shared/Calculator.cs
+++ Lrr.Shared/Calculator.cs
@@ -20,8 +20,8 @@ namespace Lrr.Shared
 
 
             auctionDetails.EndDate = inputData.RegistrationDate.AddMonths(configuration.AuctionDuration);
-            auctionDetails.StepValue = (int)Math.Ceiling(inputData.PurchaseValue * configuration.StepValueMultiple);
-            auctionDetails.StartingValue = (int)Math.Ceiling(GetWhiteValue(inputData) * configuration.StartingValueMultiple);
+            auctionDetails.StepValue = GetStepValue(inputData, configuration);
+            auctionDetails.StartingValue = GetStartingValue(inputData, configuration);
             auctionDetails.WhiteValue = GetWhiteValue(inputData);
             auctionDetails.BlackValue = (inputData.PurchaseValue - GetWhiteValue(inputData));
                 
@@ -32,14 +32,89 @@ namespace Lrr.Shared
             for (int i = 0; i <= 100; i++)
             {
                 int val = auctionDetails.StartingValue + (i * auctionDetails.StepValue);
-                validValues.Add(i, AuctionDetails.FmtIntVal(val));
+                validValues.Add(i, "₹ " + AuctionDetails.FmtIntVal(val));
             }
             auctionDetails.ValidAuctionBidValues = validValues;
 
 
         }
 
-        private int GetWhiteValue(IInputData inputData)
+        public IGiveUpPaymentSchedule GetGiveUpPaymentSchedule(IInputData inputData)
+        {
+            var rtnVal = new GiveUpPaymentSchedule();
+
+            var newPurchaseValue = GetNewPurchaseValue(inputData, configuration);
+            var oldPurchaseValue = GetWhiteValue(inputData);
+            var increasedPurchaseValue = newPurchaseValue - oldPurchaseValue;
+
+            var provIncentive = (int)Math.Ceiling(increasedPurchaseValue * configuration.GiveUpProvBuyerIncentiveRatio);
+            var sellerAddProfit = (int)Math.Ceiling(increasedPurchaseValue * configuration.GiveUpSellerIncentiveRatio);
+            var brokerage = (int)Math.Ceiling(increasedPurchaseValue * configuration.Brokerage);
+
+            rtnVal.BidderPay = newPurchaseValue;
+            rtnVal.BidderFees = GetFees(newPurchaseValue, configuration);
+                        
+            rtnVal.ProvBuyerCapitalRefund = oldPurchaseValue;
+            rtnVal.ProvBuyerFeesRefund = GetFees(oldPurchaseValue, configuration);
+            rtnVal.ProvBuyerIncentive = provIncentive;
+
+            rtnVal.OriginalSellerProfit = sellerAddProfit;
+            rtnVal.BrokerReceive = brokerage;
+            rtnVal.GovernmentAddlProfit = increasedPurchaseValue - (provIncentive + sellerAddProfit + brokerage);
+
+            return rtnVal;
+        }
+
+        public IMatchPaymentSchedule GetMatchPaymentSchedule(IInputData inputData)
+        {
+            var rtnVal = new MatchPaymentSchedule();
+            var newPurchaseValue = GetNewPurchaseValue(inputData, configuration);
+            var oldPurchaseValue = GetWhiteValue(inputData);
+            var increasedPurchaseValue = newPurchaseValue - oldPurchaseValue;
+
+            rtnVal.ProvBuyerAddCapital = increasedPurchaseValue;
+            rtnVal.ProvBuyerAddCapitalFee = GetFees(newPurchaseValue, configuration) - GetFees(oldPurchaseValue, configuration);
+            
+
+            var bidderIncentive = (int)Math.Ceiling(increasedPurchaseValue * configuration.MatchBidderIncentiveRatio);
+            var sellerAddProfit = (int)Math.Ceiling(increasedPurchaseValue * configuration.MatchSellerIncentiveRatio);
+            var brokerage = (int)Math.Ceiling(increasedPurchaseValue * configuration.Brokerage);
+
+            rtnVal.BrokerReceive = brokerage;
+            rtnVal.OriginalSellerProfit = sellerAddProfit;
+            rtnVal.BidderIncentive = bidderIncentive;
+
+            rtnVal.GovernmentAddlProfit = increasedPurchaseValue - (bidderIncentive + sellerAddProfit + brokerage);
+
+            return rtnVal;
+        }
+
+        private static int GetFees(int value, IConfiguration configuration)
+        {
+            var stampCharge = value * configuration.StampChargePercentage;
+            var regFees = value * configuration.RegistrationChargePercentage;
+            return (int) Math.Ceiling( stampCharge + regFees);
+        }
+
+        private static int GetNewPurchaseValue(IInputData inputData, IConfiguration configuration)
+        {
+            var stepValue = GetStepValue(inputData, configuration);
+            var startingValue = GetStartingValue(inputData, configuration);
+            var newPurchaseValue = startingValue + (inputData.HighestBidIndex * stepValue);
+            return newPurchaseValue;
+        }
+
+        private static int GetStartingValue(IInputData inputData, IConfiguration configuration)
+        {
+            return (int)Math.Ceiling(GetWhiteValue(inputData) * configuration.StartingValueMultiple);
+        }
+
+        private static int GetStepValue(IInputData inputData, IConfiguration configuration)
+        {
+            return (int)Math.Ceiling(inputData.PurchaseValue * configuration.StepValueMultiple);
+        }
+
+        private static int GetWhiteValue(IInputData inputData)
         {
             ulong tmp = (ulong)inputData.PurchaseValue * (ulong) inputData.WhiteBlackRatio;
             
--- Lrr.Shared/IConfiguration.cs
+++ Lrr.Shared/IConfiguration.cs
@@ -9,9 +9,21 @@ namespace Lrr.Shared
     public interface IConfiguration
     {
         
-        public double StampChargePercentage { get; set; }
-        public double RegistrationChargePercentage { get; set; }
+        public double StampChargePercentage { get;  }
+        public double RegistrationChargePercentage { get;  }
 
-        
+        public int AuctionDuration { get;  }
+        public double StartingValueMultiple { get;  }
+        public double StepValueMultiple { get;  }
+
+
+        public double GiveUpProvBuyerIncentiveRatio { get; }
+        public double GiveUpSellerIncentiveRatio { get; }
+        public double Brokerage { get; }
+
+        public double MatchSellerIncentiveRatio { get; }
+        public double MatchBidderIncentiveRatio { get; }
+
+
     }
 }
