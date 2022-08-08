8bf699542f37c2113a9fdbe61bc6d165640fa0d3
VetriThaiTha <80635114+VetriThaiTha@users.noreply.github.com>
2022-08-08 16:25:59 +0100

    Features/adhoc (#21)
    * home page changes.
    
    * styling.
    
    * Configure links
    
    * with comments
    
    * discription.

---
+++ BlazorWebAssemply/Models/Constants.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lrr.Models
+{
+    public class Constants
+    {
+        public static string Url_Document_Principle = "https://medium.com/@@thaithavetri/land-registration-reform-principle-b267413b7365";
+        public static string Url_Document_Plan = "https://medium.com/@@thaithavetri/plan-details-cfc7f89c01a4";
+        public static string Url_Document_Scenario = "https://medium.com/@@thaithavetri/scenarios-land-registration-reform-c6c0e0b8047a";
+        public static string Url_Document_References = "https://medium.com/@thaithavetri/references-36670c187a0a";
+
+
+        public static string Url_Video_Main = "https://www.youtube.com/watch?v=wXPBNq0mb7I&list=PL0p_U_-Or0_P31P751VvT81OwYEDX0KyC&index=2";
+        public static string Url_Video_Faq = "https://www.youtube.com/watch?v=TBtxBxYsUvU&list=PL0p_U_-Or0_P31P751VvT81OwYEDX0KyC&index=3";
+        public static string Url_Video_Details = "https://www.youtube.com/watch?v=-SiythpXx1U&list=PL0p_U_-Or0_P31P751VvT81OwYEDX0KyC&index=4";
+
+        
+
+    }
+}
