cd449cfef23d2569f3649532f2a8673d68e5a9d2
VetriThaiTha <80635114+VetriThaiTha@users.noreply.github.com>
2022-08-14 11:16:44 +0100

    published document links (#23)

--- BlazorWebAssemply/Models/Constants.cs
+++ BlazorWebAssemply/Models/Constants.cs
@@ -8,9 +8,9 @@ namespace Lrr.Models
 {
     public class Constants
     {
-        public static string Url_Document_Principle = "https://medium.com/@@thaithavetri/land-registration-reform-principle-b267413b7365";
-        public static string Url_Document_Plan = "https://medium.com/@@thaithavetri/plan-details-cfc7f89c01a4";
-        public static string Url_Document_Scenario = "https://medium.com/@@thaithavetri/scenarios-land-registration-reform-c6c0e0b8047a";
+        public static string Url_Document_Principle = "https://medium.com/@thaithavetri/land-registration-reform-principle-b267413b7365?source=friends_link&sk=4bb8f01b0f9714e30ec1b230284cd67d";
+        public static string Url_Document_Plan = "https://medium.com/@thaithavetri/plan-details-cfc7f89c01a4?source=friends_link&sk=49bc148c6ca8802ef12778de16aa295a";
+        public static string Url_Document_Scenario = "https://medium.com/@thaithavetri/scenarios-land-registration-reform-c6c0e0b8047a?source=friends_link&sk=37192b61d9d271d1219e5946c3720d71";
         public static string Url_Document_References = "https://medium.com/@thaithavetri/references-36670c187a0a";
 
 
